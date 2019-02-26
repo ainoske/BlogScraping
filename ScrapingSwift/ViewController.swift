a628be2b790bd9d0512d3593fdfdd841d1c6bfce
Ryusei Takano <s1250034@gmail.com>
2019-02-26 13:28:44 +0900

    Initial Commit

---
+++ ScrapingSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrapingSwift
+//
+//  Created by 高野隆正 on 2019/02/26.
+//  Copyright © 2019 高野隆正. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
